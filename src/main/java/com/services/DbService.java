f3c80ea9c68f4d3ced36a98ff0904c227e521ad6
Fabio Guimaraes <guimaraes.familia2018@gmail.com>
2020-03-09 20:57:25 -0300

    Alteração no DbService

--- src/main/java/com/services/DbService.java
+++ src/main/java/com/services/DbService.java
@@ -22,13 +22,13 @@ public class DbService {
 
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		_categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
-
-		_categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-
+	
 		cat1.getProdutos().addAll(Arrays.asList(p1, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 
@@ -36,6 +36,7 @@ public class DbService {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
+	
 		_produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
 	}
