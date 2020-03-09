e59078dd8c8f57491e0c4188e9d6cb9dc3e2852a
Fabio Guimaraes <guimaraes.familia2018@gmail.com>
2020-03-09 20:46:12 -0300

    Criado e implementado: ProdutoRepository e alteração na classe DbService

--- src/main/java/com/services/DbService.java
+++ src/main/java/com/services/DbService.java
@@ -6,20 +6,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.domain.Categoria;
+import com.domain.Produto;
 import com.repositories.CategoriaRepository;
+import com.repositories.ProdutoRepository;
 
 @Service
 public class DbService {
 
 	@Autowired
 	private CategoriaRepository _categoriaRepository;
+	@Autowired
+	private ProdutoRepository _produtoRepository;
 
 	public void instanciateTestDataBase() {
 
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
 
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
 		_categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+		_produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
 	}
 }
