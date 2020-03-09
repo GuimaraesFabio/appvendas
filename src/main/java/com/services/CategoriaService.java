b7a4e1cc60b0203e24e1d93bca4525faeeb99051
Fabio Guimaraes <guimaraes.familia2018@gmail.com>
2020-03-09 20:10:02 -0300

    Criado e implementado: Metodo findById nas classes CategoriaService e CategoriaResource

--- src/main/java/com/services/CategoriaService.java
+++ src/main/java/com/services/CategoriaService.java
@@ -1,12 +1,14 @@
 package com.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.domain.Categoria;
 import com.repositories.CategoriaRepository;
+import com.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +18,11 @@ public class CategoriaService {
 
 	public List<Categoria> findAll() {
 		return _repository.findAll();
+	}
+
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = _repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 
 }
