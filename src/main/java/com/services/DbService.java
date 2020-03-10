aab23250b8dcae092946dd81d8222eac3eed5c1c
Fabio Guimaraes <guimaraes.familia2018@gmail.com>
2020-03-10 16:49:21 -0300

    Implementado: Intefaces Estado e Cidade e alteração na classe DbService

--- src/main/java/com/services/DbService.java
+++ src/main/java/com/services/DbService.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.domain.Categoria;
+import com.domain.Cidade;
+import com.domain.Estado;
 import com.domain.Produto;
 import com.repositories.CategoriaRepository;
+import com.repositories.CidadeRepository;
+import com.repositories.EstadoRepository;
 import com.repositories.ProdutoRepository;
 
 @Service
@@ -17,18 +21,22 @@ public class DbService {
 	private CategoriaRepository _categoriaRepository;
 	@Autowired
 	private ProdutoRepository _produtoRepository;
+	@Autowired
+	private EstadoRepository _estadoRepository;
+	@Autowired
+	private CidadeRepository _cidadeRepository;
 
 	public void instanciateTestDataBase() {
 
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
-		
+
 		_categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
-	
+
 		cat1.getProdutos().addAll(Arrays.asList(p1, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 
@@ -36,8 +44,19 @@ public class DbService {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-	
 		_produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		_estadoRepository.saveAll(Arrays.asList(est1, est2));
+		_cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 }
