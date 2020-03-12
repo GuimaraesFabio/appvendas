f65de8857d9cf436d3b88c1833b5705c94cb4427
Fabio Guimaraes <guimaraes.familia2018@gmail.com>
2020-03-12 16:54:55 -0300

    Criado e implementado: Metodo findPage nas classes ClienteService e ClienteResource

--- src/main/java/com/resources/ClienteResource.java
+++ src/main/java/com/resources/ClienteResource.java
@@ -3,13 +3,16 @@ package com.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.domain.Cliente;
+import com.dto.ClienteDto;
 import com.services.ClienteService;
 
 @RestController
@@ -29,5 +32,16 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> findById(@PathVariable Integer id) {
 		Cliente obj = _service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@GetMapping(value = "/page")
+	public ResponseEntity<Page<ClienteDto>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linePerPage", defaultValue = "24") Integer linePerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy) {
+
+		Page<Cliente> list = _service.findPage(page, linePerPage, direction, orderBy);
+		Page<ClienteDto> listDto = list.map(obj -> new ClienteDto(obj));
+		return ResponseEntity.ok().body(listDto);
 	}
 }
--- src/main/java/com/services/ClienteService.java
+++ src/main/java/com/services/ClienteService.java
@@ -4,9 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.domain.Cliente;
+import com.dto.ClienteDto;
 import com.repositories.ClienteRepository;
 import com.services.exceptions.ObjectNotFoundException;
 
@@ -25,4 +29,12 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 
+	public Page<Cliente> findPage(Integer page, Integer linePerPage, String direction, String orderBy) {
+		PageRequest pageRequest = PageRequest.of(page, linePerPage, Direction.valueOf(direction), orderBy);
+		return _repository.findAll(pageRequest);
+	}
+
+	public Cliente fromDto(ClienteDto objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
 }
